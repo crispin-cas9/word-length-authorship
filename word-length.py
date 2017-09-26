ddd8c7c88e4d4cbd16d9aee48ecba5d7e2f80d22
Finley <starkwafie@gmail.com>
2017-09-26 14:20:29 -0700

    cleaned up the code!

--- word-length.py
+++ word-length.py
@@ -1,36 +1,32 @@
 # Authorship/stylometry analysis using word length
 # based on a technique by TC Mendenhall (1901)
 
-import urllib2
 import re
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-shakesall = urllib2.urlopen("http://cs.stanford.edu/people/karpathy/char-rnn/shakespeare_input.txt")
-shakes = open ('data/shakes.txt')
-leir = open ('data/leir.txt')
-marlowe = open ('data/marlowe.txt')
-test = open ('data/test.txt')
-finley = open ('data/finley.txt')
-wilkins = open ('data/wilkins.txt')
-pericles = open ('data/pericleshalf.txt')
-bacon = open ('data/bacon.txt')
-oxford = open ('data/oxford.txt')
-sidneyprose = open ('data/sidneyprose.txt')
-sidneyverse = open ('data/sidneyverse.txt')
-sidney = open ('data/sidney.txt')
+# import urllib2
+# urllib2.urlopen("http://cs.stanford.edu/people/karpathy/char-rnn/shakespeare_input.txt")
 
-# add sidney verse and prose!
+authornames = ['Anon', 'Marlowe', 'Finley', 'Bacon', 'Earl of Oxford', 'Shakespeare', 'Wilkins', 'Sidney']
+textlist = ['leir', 'marlowe', 'finley', 'bacon', 'oxford', 'shakes', 'wilkins', 'sidney']
+
+filelist = []
+
+for text in textlist:
+	filelist.append(open ('data/' + text + '.txt'))
+
+
+# match all words with 1 or more caps: [A-Z]{2}[A-Z]* (substitute that with a space)
+# then match all words and use that as the data
 
 def strip(text):
 	rawdata = text.read()
 	data = re.sub(r"[A-Z]{2}[A-Z]*", ' ', rawdata)
 	data = re.findall(r"[a-zA-Z']+", data)
 	return data
-
-# match all words with 1 or more caps: [A-Z]{2}[A-Z]*
 
 # in findper:
 # numlist is the list of word lengths. lenlist is how many words have a certain length. the second value in this list is how many words are 1 letter long, the third value is how many words are 2 letters long, etc
@@ -62,29 +58,18 @@ def findper(text):
 	return perlist
 
 
-# for each new author added: add a variable for the findper output of the text, a number to "range," the author name to "authorlist," the findper variable to percenttotal, categories
-
-per_shakes = findper(shakes)
-per_leir = findper(leir)
-per_marlowe = findper(marlowe)
-per_finley = findper(finley)
-per_wilkins = findper(wilkins)
-per_pericles = findper(pericles)
-per_bacon = findper(bacon)
-per_oxford = findper(oxford)
-per_sverse = findper(sidneyverse)
-per_sprose = findper(sidneyprose)
-per_sidney = findper(sidney)
-
 letters_per_word = []
+percenttotal = []
 authorlist = []
 
-for x in range(8):
+for author in authornames:
+	authorlist = authorlist + [author for x in range(12)]
+
+for x in range(len(authornames)):
 	letters_per_word = letters_per_word + [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
-authorlist = ['Anon' for x in range(12)] + ['Marlowe' for x in range(12)] + ['Finley' for x in range(12)] + ['Bacon' for x in range(12)] + ['Earl of Oxford' for x in range(12)] + ['Shakespeare' for x in range(12)] + ['Wilkins' for x in range(12)] + ['Sidney' for x in range(12)]
-
-percenttotal = per_leir + per_marlowe + per_finley + per_bacon + per_oxford + per_shakes + per_wilkins + per_sidney
+for file in filelist:
+	percenttotal = percenttotal + findper(file)
 
 graphdict = {'letters per word': letters_per_word, 'percent of total words': percenttotal, 'author': authorlist}
 
@@ -100,6 +85,4 @@ g = sns.factorplot(x="letters per word", y="percent of total words", hue="author
 g.despine(left=True)
 
 plt.show()
-
-
 
